7e8a70153a7519dce7070061933f6f46136fd77f
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2024-03-23 20:13:14 +0100

    Create bof1.c

---
+++ Szemeszter_2024/BoF/bof1.c
@@ -0,0 +1,15 @@
+#include <xc.h>
+#include <string.h>
+
+void masol(char* par)
+{
+    char t2[2];
+    strcpy(t2, par);
+}
+
+main()
+{
+    char t1[] = {'a','b','c','d','e'};
+    while(1)
+        masol(t1);
+}
